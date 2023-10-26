e399492f86f0ba77e9481ac5c8d7d704a1aac998
MD Ashikur Rahman <infoashikur887@gmail.com>
2023-10-27 03:27:59 +0600

    Admin Setup incomplate 7 Done

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,11 +4,41 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
-        return view('admin.admin_dashboard');
+        return view('admin.index');
+    }
+
+    //Admin Login
+    public function AdminLogin()
+    {
+        return view('admin.admin_login');
+    }
+
+
+    //logout
+    public function AdminDestroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+
+    //Admin Profile
+    public function AdminProfile(Request $request)
+    {
+        $id=Auth::user()->id;
+        $adminData=User::find($id);
+
+        return view('admin.admin_profile_view',compact('adminData'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Admin
 Route::middleware(['auth','role:admin'])->group(function () {
     Route::get('/admin/dashboard',[AdminController::class,'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout',[AdminController::class,'AdminDestroy'])->name('admin.logout');
+    Route::get('/admin/profile',[AdminController::class,'AdminProfile'])->name('admin.profile');
 });
 
+
+
+
+
+
+
+
+
+
+//vendor
 Route::middleware(['auth','role:vendor'])->group(function () {
     Route::get('/vendor/dashboard',[VendorController::class,'VendorDashboard'])->name('vendor.dashboard');
 });
 
 
+
+Route::get('/admin/login',[AdminController::class,'AdminLogin']);
 
 
 Route::get('/dashboard', function () {
